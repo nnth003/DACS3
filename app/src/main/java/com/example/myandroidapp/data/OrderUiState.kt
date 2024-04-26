53a5aa9b4d21da3f75e5f8f1a80572c31d7a3ad0
haint Nguyen Thanh Hai <hainguyenth2003@gmail.com>
2024-04-26 21:58:28 +0700

    Commit

--- app/src/main/java/com/example/myandroidapp/data/OrderUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myandroidapp.data
-
-data class OrderUiState(
-    val quantity: Int = 0,
-    val flavor: String = "",
-    val date: String = "",
-    val price: Any = "",
-    val pickupOptions: List<String> = listOf()
-)
--- app/src/main/java/com/example/myandroidapp/ui/GameUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myandroidapp.ui.theme
-
-data class GameUiState(
-    val currentScrambleWord: String = "",
-    val currentWordCount: Int = 1,
-    val score: Int = 0,
-    val isGuessedWordWrong: Boolean = false,
-    val isGameOver: Boolean = false
-)
